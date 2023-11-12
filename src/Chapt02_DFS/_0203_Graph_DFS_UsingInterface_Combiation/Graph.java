7ace488ac3831ec2c08512e1be0308e5805628f5
Chufeng JIANG <jiangchufengjcf@163.com>
2023-11-11 16:05:21 -0800

    mst

--- src/Chapt02_DFS/_0203_Graph_DFS_UsingInterface_Combiation/Graph.java
+++
@@ -1,9 +0,0 @@
-package Chapt02_DFS._0203_Graph_DFS_UsingInterface_Combiation;
-public interface Graph {
-
-    int V();
-    int E();
-    boolean hasEdge(int v, int w);
-    Iterable<Integer> adj(int v);
-    int degree(int v);
-}
